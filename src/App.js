3c1725a933676ba04b197bdb47bd558bc1b61be6
dylan-rinella <rinelladylan@aim.com>
2021-01-28 17:27:20 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,57 +1,78 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+class App extends React.Component {
+  state = {
+    cardInfo: [],
+    userInput: ''
+    
+  }
+  componentDidMount(){
+    axios.get('https://api.github.com/users/dylan-rinella')
+          .then(res => {
+              this.setState({
+                  cardInfo: res.data,
+                  gitImg: res.data.avatar_url,
+                  gitId: res.data.id
+              })
+           })
+          .catch(err => {
+              console.log(err)
+           })
+  }
+
+  handleChange = e => {
+    this.setState({
+        userInput: e.target.value
+    })
+  }
+  
+  handleClick = e => {
+    e.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.userInput}`)
+            .then(res => {
+                this.setState({
+                    cardInfo: res.data,
+                    gitImg: res.data.avatar_url,
+                    gitId: res.data.id
+                })
+            }) 
+  }
+
+  render(){
+    return (
+    <>
+      <div className='git-profile-container'>
+        <h2>GitHub User Info</h2>
+        <img src={this.state.gitImg} key={this.state.gitId} alt='img' />
+        <h3>{this.state.cardInfo.name}</h3>
+        <p>GitHub User Id: {this.state.gitId}</p>
+        <p>Following: {this.state.cardInfo.following}</p>
+        <p>Followers: {this.state.cardInfo.followers}</p>
+        <p>Twitter: {this.state.cardInfo.twitter_username}</p>
+        <p>Public Repo Amount: {this.state.cardInfo.public_repos}</p>
+
+        <h3>Search for Users</h3>
+        <form>
+          <input onChange={this.handleChange} type='text' />
+          <button onClick={this.handleClick}>Find User</button>
+
+        </form>
+      
+      </div>
+    </>
+    )
+  }
+
+
+
+
+
+
+
+
 }
 
 export default App;
+
